298e351f132aff5a7b63a2bd43e52f2975d70e48
Ashwani Prataap Singh <98383487+cheetah7027@users.noreply.github.com>
2026-05-19 00:07:50 +0530

    fix: update vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/idea-metric/',
+  base: process.env.VERCEL ? '/' : '/idea-metric/',
   plugins: [react()],
 })
